ca7c4f1db7841b08a3ad45f63a6132081e7e8fd7
nemhon <namhhdev024@gmail.com>
2026-06-26 00:31:23 +0700

    validate input date

--- src/main/java/com/capstone/su26_sep490_g2_be/enums/ErrorCode.java
+++ src/main/java/com/capstone/su26_sep490_g2_be/enums/ErrorCode.java
@@ -70,6 +70,7 @@ public enum ErrorCode {
 	// Tournament participation
 	TOURNAMENT_FULL("TOURNAMENT_001", "Giải đấu đã đủ số người tham gia", HttpStatus.CONFLICT),
 	PARTICIPANT_INVALID_EXCEL("TOURNAMENT_002", "File không hợp lệ. Vui lòng tải mẫu từ nút \"Tải template\" hoặc upload file .xlsx / .csv", HttpStatus.BAD_REQUEST),
+	TOURNAMENT_DATE_INVALID("TOURNAMENT_003", "Ngày tháng giải đấu không hợp lệ", HttpStatus.BAD_REQUEST),
 
 	// Payment
 	PAYMENT_CREATE_FAILED("PAYMENT_001", "Tạo đơn thanh toán thất bại", HttpStatus.BAD_GATEWAY),
